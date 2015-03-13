df395e709dd91b5aa7ac46837ae8ec415d9066aa
leiucky <leichenyi1@163.com>
2015-03-13 16:12:18 +0800

    o(n)

--- [84]LargestRectangleinHistogram.cpp
+++ [84]LargestRectangleinHistogram.cpp
@@ -1,3 +1,4 @@
+//暴力 o(n2)
 class Solution {
 public:
     int largestRectangleArea(vector<int> &height) {
@@ -32,5 +33,29 @@ public:
         }
         
         return max;
+    }
+};
+
+
+//stack,o(n)
+
+class Solution {
+public:
+    int Max(int a, int b){return a > b ? a : b;}
+    int largestRectangleArea(vector<int> &height) {
+    	height.push_back(0);
+        stack<int> stk;
+        int i = 0;
+        int maxArea = 0;
+        while(i < height.size()){
+            if(stk.empty() || height[stk.top()] <= height[i]){
+                stk.push(i++);
+            }else {
+                int t = stk.top();
+				stk.pop();
+                maxArea = Max(maxArea, height[t] * (stk.empty() > 0 ? i : i - stk.top() - 1));
+            }
+        }
+        return maxArea;
     }
 };
